3371e4eaab941aa4e7b68e522a66e37256bda4b8
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2023-08-20 21:22:23 -0600

    Custom Hooks

--- src/01-useState/CounterWithCustomHook.jsx
+++ src/01-useState/CounterWithCustomHook.jsx
@@ -1,22 +1,19 @@
-import { useCounter } from '../Hooks/useCounter'
+import { useCounter } from '../Hooks/useCounter';
 
 
 export const CounterWithCustomHook = () => {
   
-    const { counter } = useCounter();
+    const { counter, increment, decrement, reset } = useCounter();
   
     return (
-
     <> 
-    <h1>Counter Custom Hook: { counter } </h1>
-    <hr/>
+        <h1>Counter Custom Hook: { counter } </h1>
+        <hr/>
 
-    <button className="btn btn-primary"> +1 </button>
-    <button className="btn btn-secondary">Reset</button>
-    <button className="btn btn-success"> -1 </button>
-
+        <button className="btn btn-primary" onClick={ () => increment(2) }> +1 </button>
+        <button className="btn btn-secondary" onClick={ reset }>Reset</button>
+        <button className="btn btn-success" onClick={ () => decrement(2) }> -1 </button>
     </>
-
   )
 }
 
--- src/Hooks/useCounter.js
+++ src/Hooks/useCounter.js
@@ -5,9 +5,26 @@ export const useCounter = ( initialValue = 10 ) => {
 
     const [counter, setCounter] = useState( initialValue );
 
+    const increment = ( value = 1 ) => {
+        setCounter( counter + value );
+    }
+
+    const decrement = ( value = 1 ) => {
+        // if (counter === 0) return;
+        setCounter( counter - value );
+    }
+
+    const reset = () => {
+        setCounter( initialValue );
+    }
+
     return {
 
-        counter: counter,
+        // counter: counter,    //despues de ECMA6, si la propiedad tiene el mismo nombre de la variable de pone como abajo
+        counter,
+        increment,
+        decrement,
+        reset,
 
     }
 
