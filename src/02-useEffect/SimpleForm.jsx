bfbec5579f19d247eb2436f02a18130e9d400e2d
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2023-08-23 04:39:14 -0600

    UseEffect practicing

--- src/02-useEffect/SimpleForm.jsx
+++ src/02-useEffect/SimpleForm.jsx
@@ -1,23 +1,31 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export const SimpleForm = () => {
 
     const [formState, setFormState] = useState ({
         username: 'Maria',
         email: 'maria@google.com'
-
-
-
-
-    })
+    });
        
     
+    const {  username, email } = formState;
 
+    const onInputChange = ( {target} ) => {
+        const { name, value } = target;
+        // console.log( { name, value }  );
+        setFormState({
+            ...formState,
+            [ name ]: value
+        });
+    }
+
+    useEffect( () => {
+        console.log(' El useEffect se llamo');
+    });
 
 
   return (
     <>
-    
         <h1>Simple Form</h1>
         <hr />
 
@@ -25,18 +33,20 @@ export const SimpleForm = () => {
             type="text" 
             className="form-control"
             placeholder="username"
-            name="username"          
+            name="username"     
+            value={ username }   
+            onChange={ onInputChange }  
         />
 
 <input 
             type="email" 
             className="form-control mt-2"
             placeholder="mrestrella4@gmail.com"
-            name="email"          
+            name="email"   
+            value={ email }       
+            onChange={ onInputChange }
         />
-    
-    
-    
+      
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,16 @@ import './index.css';
 import { SimpleForm } from './02-useEffect/SimpleForm';
 // import { CounterApp } from './01-useState/CounterApp';
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
-
+SimpleForm
 // import { HooksApp } from './HooksApp';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    {/* <HooksApp /> */}
-    {/* <CounterApp /> */}
-    {/* <CounterWithCustomHook /> */}
+  // <React.StrictMode>
+    // {/* <HooksApp /> */}
+    // {/* <CounterApp /> */}
+    // {/* <CounterWithCustomHook /> */}
 
     <SimpleForm />
-  </React.StrictMode>,
+  // </React.StrictMode>
 )
